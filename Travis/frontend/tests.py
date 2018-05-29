57389a11abe463daa2c3eae5ac126244529ccfd7
Charlotte Blake <charlotte.p.blake@gmail.com>
2018-05-29 10:32:32 +0100

    Add tests

--- Travis/frontend/tests.py
+++ Travis/frontend/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class RandomTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_pass(self):
+        """This test should pass"""
+        self.assertTrue(1 == 1)
+
+    def test_fail(self):
+        """This test should fail"""
+        self.assertTrue(1 == 2)
